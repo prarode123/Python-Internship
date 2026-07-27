e78983587202f513f6587e2862bb0065695c3085
Pratiksha Rode <rode2020pra@gmail.com>
2026-07-27 18:11:00 +0530

    Completed CRUD operations

--- student/urls.py
+++ student/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('students/', views.students, name='students'),
+    path('add-student/', views.add_student, name='add_student'),
+    path('edit-student/<int:id>/', views.edit_student, name='edit_student'),
+    path('delete-student/<int:id>/', views.delete_student, name='delete_student'),
 ]
--- student/views.py
+++ student/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student
 
 
@@ -20,3 +20,44 @@ def students(request):
     return render(request, 'student/students.html', {
         "students": student_list
     })
+
+
+def add_student(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        course = request.POST['course']
+        email = request.POST['email']
+
+        Student.objects.create(
+            name=name,
+            course=course,
+            email=email
+        )
+
+        return redirect('students')
+
+    return render(request, 'student/add_student.html')
+
+
+def edit_student(request, id):
+    student = Student.objects.get(id=id)
+
+    if request.method == "POST":
+        student.name = request.POST['name']
+        student.course = request.POST['course']
+        student.email = request.POST['email']
+
+        student.save()
+
+        return redirect('students')
+
+    return render(request, 'student/edit_student.html', {
+        'student': student
+    })
+
+
+def delete_student(request, id):
+    student = Student.objects.get(id=id)
+    student.delete()
+
+    return redirect('students')
